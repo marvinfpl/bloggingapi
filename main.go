7b475d6a9d9e4068b870192a1a3668354fcf0011
marvinfpl <marvinfoppolo@gmail.com>
2024-11-02 15:43:10 +0100

    end

--- main.go
+++ main.go
@@ -3,96 +3,102 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/labstack/echo/v4"
 	"time"
-	"strconv"
+	"github.com/lib/pq"
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+var (
+	db, _ = gorm.Open(sqlite.Open("bloggingDatabase.db"), &gorm.Config{})
+)
 type Post struct {
-	ID int				`json:"id"`
+	ID uint				`json:"id"`
 	Title string 		`json:"title"`
 	Content string 		`json:"content"`
 	Category string		`json:"category"`
-	Tags []string 		`json:"tags"`
-	CreatedAt time.Time	`json:"createdAt"`
-	UpdatedAt time.Time	`json:"updatedAt"`
-}
-
-var posts = []Post{
-	{ID: 1, Title: "first post", Content: "none", Category: "coding", Tags: []string{"coding with marvin"},CreatedAt: time.Now() },
+	Tags pq.StringArray	`gorm:"type:text[]" json:"tags"`
+	CreatedAt time.Time	`gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time	`gorm:"autoUpdateTime" json:"updatedAt"`
 }
 
 func main() {
 	e := echo.New()
 
+	db.AutoMigrate(&Post{})
+
 	fmt.Println("Blog running on port 8080...")
 
-	e.POST("/posts", createPost)
-	e.PUT("/posts/:id", updatePost)
-	e.DELETE("/posts/:id", deletePost)
-	e.GET("/posts/:id", getPost)
-	e.GET("/posts", getAllPosts)
+	e.POST("/posts", CreateBlogPost)
+	e.PUT("/posts/:id", UpdateBlogPost)
+	e.DELETE("/posts/:id", DeleteBlogPost)
+	e.GET("/posts/:id", GetBlogPost)
+	e.GET("/posts", GetAllBlogPosts)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func createPost(c echo.Context) error {
-	post := new(Post)
-	if err := c.Bind(post); err != nil {
+func CreateBlogPost(c echo.Context) error {
+	var post Post
+
+	if err := c.Bind(&post); err != nil {
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": err})
 	}
-	post.CreatedAt = time.Now()
-	posts = append(posts, *post)
-	return c.JSON(http.StatusCreated, post)
+
+	if err := db.Create(&post).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err})
+	}
+
+	return c.JSON(http.StatusOK, post)
 }
 
-func updatePost(c echo.Context) error {
-	idStr := c.Param("/posts/:id")
-	if idStr == "" {
+func UpdateBlogPost(c echo.Context) error {
+	var post Post
+
+	id := c.Param("id")
+
+	if err := db.Where("ID = ?", id).First(&post).Error; err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": err})
+	}
+
+	if err := c.Bind(&post); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": err})
+	}
+
+	db.Save(&post)
+	return c.JSON(http.StatusOK, post)
+}
+
+func DeleteBlogPost(c echo.Context) error {
+	var post Post
+	id := c.Param("id")
+	if err := db.Where("ID = ?", id).Delete(&post).Error; err != nil {
 		return c.JSON(http.StatusNotFound, echo.Map{"error": "post not found"})
 	}
-	id, _ := strconv.Atoi(idStr)
-	for _, post := range posts{
-		if post.ID == id {
-			if err := c.Bind(&post); err != nil {
-				return c.JSON(http.StatusBadRequest, echo.Map{"error": err})
-			}
-			post.UpdatedAt = time.Now()
-			return c.JSON(http.StatusOK, post)
-		}
-	}
-	return c.JSON(http.StatusNotFound, echo.Map{"error": "post not found"})
+	return c.JSON(http.StatusNoContent, echo.Map{})
 }
 
-func deletePost(c echo.Context) error {
-	idStr := c.Param("/posts/:id")
-	if idStr == "" {
-		return c.JSON(http.StatusNotFound, echo.Map{"error": "post not found"})		
+func GetBlogPost(c echo.Context) error {
+	var post Post
+	id := c.Param("id")
+	if err := db.Where("ID = ?", id).First(&post).Error; err != nil {
+		return c.JSON(http.StatusNotFound, echo.Map{"error": "post not found"})
 	}
-	id, _ := strconv.Atoi(idStr)
-	for index, post := range posts {
-		if post.ID == id {
-			posts = append(posts[:index], posts[index+1:]... )
-			return c.JSON(http.StatusNoContent, echo.Map{"message": "post deleted"})
-		}
-	}
-	return c.JSON(http.StatusBadRequest, echo.Map{"error": "post not found"})
+	return c.JSON(http.StatusOK, post)
 }
 
-func getPost(c echo.Context) error {
-	idStr := c.Param("/posts/:id")
-	if idStr == "" {
-		return c.JSON(http.StatusNotFound, echo.Map{"error": "post not found"})		
-	}
-	id, _ := strconv.Atoi(idStr)
-	for _, post := range posts {
-		if post.ID == id {
-			return c.JSON(http.StatusOK, post)
+func GetAllBlogPosts(c echo.Context) error {
+	post := []Post{}
+	term := c.QueryParam("term")
+	if term == "" {
+		if err := db.Find(&post).Error; err != nil {
+			return c.JSON(http.StatusBadRequest, echo.Map{"error": err})
 		}
+		return c.JSON(http.StatusOK, post)
 	}
-	return c.JSON(http.StatusBadRequest, echo.Map{"error": "post not found"})
-}
-
-func getAllPosts(c echo.Context) error {
-	return c.JSON(http.StatusOK, posts)
+	if err := db.Where("Title LIKE ?", "%"+term+"%").Find(&post).Error; err != nil {
+		return c.JSON(http.StatusNotFound, echo.Map{"error": "post not found"})
+	}
+	return c.JSON(http.StatusOK, post)
 }
